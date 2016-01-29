ddf94979d8f8fa13ace944c95d6b95d072160569
liebesu <lichunjiang42@gmail.com>
2016-01-29 13:04:33 +0800

    4096

--- Grep.py
+++ Grep.py
@@ -1,9 +1,10 @@
 __author__ = 'liebesu'
 import os
-path=
-input=
+input='/polydata/content/malware/md5/md5-info.sys'
+outpath='/polydata/content/malware/md5/info4096/'
 a=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 for i in a:
     for j in a:
-        cmd="grep '^"+i+j+"' "+input +"> " + path+i+j
-        os.system(cmd)
+        for m in a:
+            cmd="grep '^"+i+j+m+"' "+input +"> " +outpath+i+j+m
+            os.system(cmd)
