b4a1e2e14dd1f5adac726127b48834a25ba15f42
liebesu <lichunjiang42@gmail.com>
2016-01-29 14:50:43 +0800

    use pool

--- Grep.py
+++ Grep.py
@@ -1,10 +1,20 @@
 __author__ = 'liebesu'
 import os
-input='/polydata/content/malware/md5/md5-info.sys'
-outpath='/polydata/content/malware/md5/info4096/'
-a=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-for i in a:
-    for j in a:
-        for m in a:
-            cmd="grep '^"+i+j+m+"' "+input +"> " +outpath+i+j+m
+from multiprocessing import Pool
+global input,outpath,c
+
+input=''
+outpath=''
+def grep(b):
+    a=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    for i in a:
+        for j in a:
+            cmd="grep '^"+i+j+b+"' "+input +"> " +outpath+i+j+b
             os.system(cmd)
+if __name__ == "__main__":
+    b=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    pool = Pool(processes=16)
+    pool.map(grep,b)
+    pool.close()
+    pool.join()
+    print "finish"
