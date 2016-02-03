097b763f6a68a61a12fd0024f1dfb4c5f3ee6e1f
liebesu <lichunjiang42@gmail.com>
2016-02-03 14:31:25 +0800

    update
    update

--- db_name_update.py
+++ db_name_update.py
@@ -1,4 +1,4 @@
-from multiprocessing.pool import Pool
+from multiprocessing import Pool
 import MySQLdb
 import os
 import time
@@ -11,47 +11,53 @@ __author__ = 'liebesu'
 inputpath,outputpath,Scantype,datebaseip,datebaseuser,datebasepsw,datebasename,datebasetable,md5filename,key,newav=read_conf()
 
 def db_name_update(md5):
+    md5=str(md5)
     try:
         db = MySQLdb.connect(datebaseip,datebaseuser,datebasepsw,datebasename)
-        cursor = db.cursor()
-        sql="select * from "+datebasetable+"where Md5="+md5
+        cursor = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+        sql="select * from "+datebasetable+" where Md5='"+md5+"'"
+        cursor.execute(sql)
+        result=cursor.fetchall()
+        print result
+        cursor.close()
+        db.close()
+    except:
+        time.sleep(5)
+        cursor.close()
+        db.close()
+        db = MySQLdb.connect(datebaseip,datebaseuser,datebasepsw,datebasename)
+        cursor = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+        sql="select * from "+datebasetable+" where Md5='"+md5+"'"
         cursor.execute(sql)
         result=cursor.fetchall()
         cursor.close()
         db.close()
-    except:
-            time.sleep(5)
-            cursor.close()
-            db.close()
-            db = MySQLdb.connect(datebaseip,datebaseuser,datebasepsw,datebasename)
-            cursor = db.cursor()
-            sql="select * from "+datebasetable+" where Md5='"+md5+"'"
-            cursor.execute(sql)
-            result=cursor.fetchall()
-            cursor.close()
-            db.close()
     for av in newav.replace('\n', ''):
-        if result["Virus_Name"] == "null":
+        if result.Virus_Name == "null":
+            print result['Virus_Name']
             if result[av]=='' and  "null" not in result[av] :
                 newvirusname=av+":"+result[av]
             break
 
     db = MySQLdb.connect(datebaseip,datebaseuser,datebasepsw,datebasename)
-    cursor = db.cursor()
+    cursor = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
     sql="update "+datebasetable+"set Virus_Name="+newvirusname+" where Md5 ='"+md5+"'"
     cursor.execute(sql)
     cursor.close()
     db.close()
+    print "finish"
 def allmd5():
     md5filedir = os.path.join(ROOTPATH,"md5file")
     allmd5file=os.path.join(md5filedir,md5filename)
     allmd5=open(allmd5file,"r").readlines()
     allmd5=[md5.replace('\n', '').replace('\r', '') for md5 in allmd5]
+    print len(allmd5)
     return allmd5
 
 if __name__=="__main__":
-    pool=Pool(processes=20)
-    pool.map(db_name_update,allmd5())
+    allmd5s=allmd5()
+    pool=Pool(processes=1)
+    pool.map(db_name_update,allmd5s)
     pool.close()
     pool.join()
-    print "finish"
+    print "finished"
